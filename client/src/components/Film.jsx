1040af94dfb46744e9a5de5fbdddcba25ea33bcb
hatwell <caroline.hatwell@gmail.com>
2017-06-05 18:20:45 +0100

    added files

---
+++ client/src/components/Film.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Film extends React.compoment {
+  render(){
+    return(
+      <div className="film">
+        <button className="save-film"></button>
+        <h4 className="film-title">
+          {this.props.title}
+        </h4>
+        <a className="showtimes" href={this.props.showtimes}>
+          showtimes
+        </a>
+      </div>
+    )
+  }
+
+
+}
+
+export default Film;
---
+++ client/src/components/FilmList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class FilmList extends React.Component {
+  render(){
+
+  }
+}
